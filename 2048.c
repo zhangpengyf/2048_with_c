3e344bc8848051dc244f620a68bac3ced658905f
zhangpeng <zhangpeng@interrcs.com>
2016-09-18 21:36:30 +0800

    save

--- 2048.c
+++ 2048.c
@@ -7,29 +7,88 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <time.h>
+#include <assert.h>
 #include <signal.h>
 
 #define SIZE 5
 uint32_t score = 0;
 uint8_t scheme = 0;
 
-bool findPairDown(uint8_t board[SIZE][SIZE]) {
-    bool success = false;
-    uint8_t x,y;
-    for (x = 0; x < SIZE; x++) {
-        for (y = 0; y < SIZE-1; y++) {
-            if (board[x][y] == board[x][y+1]) return true;
-        }
-    }
-    return success;
+void getColor(uint8_t value, char *color, size_t length) {
+	uint8_t original[] = {8,255,1,255,2,255,3,255,4,255,5,255,6,255,7,255,9,0,10,0,11,0,12,0,13,0,14,0,255,0,255,0};
+	uint8_t blackwhite[] = {232,255,234,255,236,255,238,255,240,255,242,255,244,255,246,0,248,0,249,0,250,0,251,0,252,0,253,0,254,0,255,0};
+	uint8_t bluered[] = {235,255,63,255,57,255,93,255,129,255,165,255,201,255,200,255,199,255,198,255,197,255,196,255,196,255,196,255,196,255,196,255};
+	uint8_t *schemes[] = {original,blackwhite,bluered};
+	uint8_t *background = schemes[scheme]+0;
+	uint8_t *foreground = schemes[scheme]+1;
+	if (value > 0) while (value--) {
+		if (background+2<schemes[scheme]+sizeof(original)) {
+			background+=2;
+			foreground+=2;
+		}
+	}
+	snprintf(color,length,"\033[38;5;%d;48;5;%dm",*foreground,*background);
 }
 
-uint8_t countEmpty(uint8_t board[SIZE][SIZE]) {
-    uint8_t x,y;
-    uint8_t count=0;
-    for (x = 0; x<SIZE; x++) {
-        for (y = 0; y < SIZE; y++) {
-            if (board[x][y]==0) {
+void drawBoard(uint8_t board[SIZE][SIZE]) {
+	uint8_t x,y;
+	char c;
+	char color[40], reset[] = "\033[m";
+	printf("\033[H");
+
+	printf("2048.c %17d pts\n\n",score);
+
+	for (y=0;y<SIZE;y++) {
+		for (x=0;x<SIZE;x++) {
+			getColor(board[x][y],color,40);
+			printf("%s",color);
+			printf("       ");
+			printf("%s",reset);
+		}
+		printf("\n");
+		for (x=0;x<SIZE;x++) {
+			getColor(board[x][y],color,40);
+			printf("%s",color);
+			if (board[x][y]!=0) {
+				char s[8];
+				snprintf(s,8,"%u",(uint32_t)1<<board[x][y]);
+				uint8_t t = 7-strlen(s);
+				printf("%*s%s%*s",t-t/2,"",s,t/2,"");
+			} else {
+				printf("   ·   ");
+			}
+			printf("%s",reset);
+		}
+		printf("\n");
+		for (x=0;x<SIZE;x++) {
+			getColor(board[x][y],color,40);
+			printf("%s",color);
+			printf("       ");
+			printf("%s",reset);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("        ←,↑,→,↓ or q        \n");
+	printf("\033[A"); // one line up
+}
+
+bool hasPair(uint8_t board[SIZE][SIZE]) {
+    int i,j;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE-1; j++) {
+            if (board[i][j] == board[i][j+1]) return true;
+        }
+    }
+    return false;
+}
+
+uint8_t getEmptyCount(uint8_t board[SIZE][SIZE]) {
+    int i,j;
+    int count=0;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            if (board[i][j]==0) {
                 count++;
             }
         }
@@ -37,12 +96,16 @@ uint8_t countEmpty(uint8_t board[SIZE][SIZE]) {
     return count;
 }
 
-bool gameEnded(uint8_t board[SIZE][SIZE]) {
+bool doesGameOver(uint8_t board[SIZE][SIZE]) {
     bool ended = true;
-    if (countEmpty(board)>0) return false;
-    if (findPairDown(board)) return false;
+    if (getEmptyCount(board) > 0) return false;
+    if (hasPair(board)) { 
+      return false;
+    }
     rotateBoard(board);
-    if (findPairDown(board)) ended = false;
+    if (hasPair(board)) {
+      ended = false;
+    }
     rotateBoard(board);
     rotateBoard(board);
     rotateBoard(board);
@@ -51,39 +114,38 @@ bool gameEnded(uint8_t board[SIZE][SIZE]) {
 
 void addRandom(uint8_t board[SIZE][SIZE]) {
     static bool initialized = false;
-    uint8_t x, y;
-    uint8_t r, len=0;
-    uint8_t n, list[SIZE*SIZE][2];
+    int i, j;
+    int r, len = 0;
+    int list[SIZE*SIZE][2];
     
     if (!initialized) {
         srand(time(NULL));
         initialized = true;
     }
     
-    for (x=0;x<SIZE;x++) {
-        for (y=0;y<SIZE;y++) {
-            if (board[x][y]==0) {
-                list[len][0]=x;
-                list[len][1]=y;
+    for (i = 0; i < SIZE; x++) {
+        for (j = 0; j < SIZE; j++) {
+            if (board[i][j] == 0) {
+                list[len][0] = i;
+                list[len][1] = j;
                 len++;
             }
         }
     }
     
-    if (len>0) {
+    if (len > 0) {
         r = rand()%len;
-        x = list[r][0];
-        y = list[r][1];
-        n = (rand()%10)/9+1;
-        board[x][y]=n;
+        i = list[r][0];
+        j = list[r][1];
+        board[i][j] = 1;//always 2
     }
 }
 
-void initBoard(uint8_t board[SIZE][SIZE]) {
-    uint8_t x,y;
-    for (x=0;x<SIZE;x++) {
-        for (y=0;y<SIZE;y++) {
-            board[x][y]=0;
+void initGame(uint8_t board[SIZE][SIZE]) {
+    int i,j;
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            board[i][j]=0;
         }
     }
     addRandom(board);
@@ -116,11 +178,11 @@ void setBufferedInput(bool enable) {
     }
 }
 
-void rotateBoard(uint8_t board[SIZE][SIZE]) {
-    uint8_t i,j,n=SIZE;
+void rotateBoardClockwise(uint8_t board[SIZE][SIZE]) {
+    int i,j,n = SIZE;
     uint8_t tmp;
-    for (i=0; i<n/2; i++) {
-        for (j=i; j<n-i-1; j++) {
+    for (i = 0; i < n/2; i++) {
+        for (j = i; j < n-i-1; j++) {
             tmp = board[i][j];
             board[i][j] = board[j][n-i-1];
             board[j][n-i-1] = board[n-i-1][n-j-1];
@@ -183,31 +245,31 @@ bool moveUp(uint8_t board[SIZE][SIZE]) {
 
 bool moveLeft(uint8_t board[SIZE][SIZE]) {
     bool moved = false;
-    rotateBoard(board);
+    rotateBoardClockwise(board);
     moved = moveUp(board);
-    rotateBoard(board);
-    rotateBoard(board);
-    rotateBoard(board);
+    rotateBoardClockwise(board);
+    rotateBoardClockwise(board);
+    rotateBoardClockwise(board);
     return moved;
 }
 
 bool moveDown(uint8_t board[SIZE][SIZE]) {
     bool moved = false;
-    rotateBoard(board);
-    rotateBoard(board);
+    rotateBoardClockwise(board);
+    rotateBoardClockwise(board);
     moved = moveUp(board);
-    rotateBoard(board);
-    rotateBoard(board);
+    rotateBoardClockwise(board);
+    rotateBoardClockwise(board);
     return moved;
 }
 
 bool moveRight(uint8_t board[SIZE][SIZE]) {
     bool moved = false;
-    rotateBoard(board);
-    rotateBoard(board);
-    rotateBoard(board);
+    rotateBoardClockwise(board);
+    rotateBoardClockwise(board);
+    rotateBoardClockwise(board);
     moved = moveUp(board);
-    rotateBoard(board);
+    rotateBoardClockwise(board);
     return moved;
 }
 
@@ -256,7 +318,7 @@ int main(int argc, const char* argv[]){
             usleep(150000);
             addRandom(board);
             drawBoard(board);
-            if (gameEnded(board)) {
+            if (doesGameOver(board)) {
                 printf("         GAME OVER          \n");
                 break;
             }
